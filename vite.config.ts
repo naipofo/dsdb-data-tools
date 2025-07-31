5670e7052cb39c0573da00b3a43e90a42384e337
naipofo <50967056+naipofo@users.noreply.github.com>
2025-07-31 09:32:37 +0200

    Fix static site routing

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig(({ command }) => ({
-  base: command === "build" ? "/dsdb-data-tools/" : "/",
+export default defineConfig({
+  base: "/dsdb-data-tools/",
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-}));
+});
